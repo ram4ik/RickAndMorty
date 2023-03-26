1aa82c42bb714e07e39f170abdecbb5064168e23
Ramill <ramill_ibragimov@icloud.com>
2023-03-26 11:40:42 +0300

    Location Pagination

--- RickAndMorty/ViewModels/Locations/RMLocationViewModel.swift
+++ RickAndMorty/ViewModels/Locations/RMLocationViewModel.swift
@@ -30,8 +30,57 @@ final class RMLocationViewModel {
     
     public private(set) var cellViewModels: [RMLocationTableViewCellViewModel] = []
     
+    public var shouldShowLoadMoreIndicator: Bool {
+        return apiInfo?.next != nil
+    }
+    
+    public var isLoadingMoreLocations = false
+    
     init() {
         
+    }
+    
+    public func fetchAdditionalLocations() {
+        
+        guard !isLoadingMoreLocations else {
+            return
+        }
+        
+        guard let nextUrlString = apiInfo?.next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreLocations = true
+        
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreLocations = false
+            return
+        }
+        
+        RMService.share.execute(request,
+                                expecting: RMGetAllLocationsResponse.self) { [weak self] result in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            switch result {
+            case .success(let responseModel):
+                let moreResults = responseModel.results
+                let info = responseModel.info
+                
+                strongSelf.apiInfo = info
+                strongSelf.cellViewModels.append(contentsOf: moreResults.compactMap({
+                    return RMLocationTableViewCellViewModel(location: $0)
+                }))
+                DispatchQueue.main.async {
+                    strongSelf.isLoadingMoreLocations = false
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+                self?.isLoadingMoreLocations = false
+            }
+        }
     }
     
     public func location(at index: Int) -> RMLocation? {
@@ -52,6 +101,7 @@ final class RMLocationViewModel {
                     self?.delegate?.didFetchInitialLocations()
                 }
             case .failure:
+                // TODO: Handle error
                 break
             }
         }
